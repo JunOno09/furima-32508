04056c178007297443452184b604ae9ba7e873a9
JunOno09 <jastis.09@gmail.com>
2020-12-15 11:25:10 +0900

    バリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :email, presence: true,uniqueness: true, format: with: (/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
-         validates :encrypted_password, presence: true,length: { minimum: 5 }, format: with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i)
 
-         with_options presence: true, format: ( with: /\A[ぁ-んァ-ヶ一-龥々]+\z/)do
+        #  validates :password, format: { with: 正規表現, message: 'エラー文' }
+          validates :nickname, presence: true
+          validates :email, presence: true,uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } 
+
+         validates :encrypted_password, presence: true,length: { minimum: 5 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+          with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
           validates :family_name, presence: true
-          validates :first_name, presence: true
+           validates :first_name, presence: true
          end
-         with_options presence: true, format: ( with: /\A[ァ-ヶー－]+\z/) do
-          validates :family_name_kana, presence: true
-          validates :first_name_kana, presence: true
-         end
+
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do 
+           validates :family_name_kana, presence: true
+           validates :first_name_kana, presence: true
+          end
          validates :birth_day, presence: true
 
 
