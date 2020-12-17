005b277efe09c25bb13534651d73d6c112930c81
JunOno09 <jastis.09@gmail.com>
2020-12-17 18:57:39 +0900

    FactoryBotの編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品' do
+    context '商品出品がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+      it "nicknameが6文字以下で登録できる" do
+      end
+      it "passwordが6文字以上であれば登録できる" do
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "nicknameが7文字以上であれば登録できない" do
+      end
+      it "emailが空では登録できない" do
+      end
+      it "重複したemailが存在する場合登録できない" do
+      end
+      it "passwordが空では登録できない" do
+      end
+      it "passwordが5文字以下であれば登録できない" do
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      end
+    end
+  end
 end
