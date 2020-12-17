3c90a6933a6ebbb917883180ff8e95460eed2bc8
JunOno09 <jastis.09@gmail.com>
2020-12-17 15:23:54 +0900

    newファイルの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,31 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
   def index
-      @items = Item.all
+    @items = Item.includes(:user)
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+   else
+      render :new
+   end
+  end
+
+   def show
+    @item = Item.find(params[:id])
+    
+   end
+
+  # private
+  # def item_params
+  #   params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id :image, :price).merge(user_id: current_user.id)
+  # end
+
+
 end
