0f38fd8e6ca3aab5ee607ec3682e925ebb036e40
JunOno09 <jastis.09@gmail.com>
2020-12-18 18:34:46 +0900

    ビューファイルの記述、並び替え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @items = Item.all
-    # @items = Item.includes(:user)
+     @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -18,10 +17,10 @@ class ItemsController < ApplicationController
    end
   end
 
-  #  def show
+  # def show
   #   @item = Item.find(params[:id])
     
-  #  end
+  # end
 
   private
   def item_params
