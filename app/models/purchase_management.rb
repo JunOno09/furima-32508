deaea37378cc83325e62ff2bbad73a865877812d
JunOno09 <jastis.09@gmail.com>
2020-12-23 14:28:55 +0900

    アクティブハッシュとかの記述

--- app/models/purchase_management.rb
+++ app/models/purchase_management.rb
@@ -1,5 +1,21 @@
-class PurchaseManagement < ApplicationRecord
-has_one :user_address
-belongs_to :item
-belongs_to :user
+class PurchaseManagement 
+
+  has_one :user_address
+  belongs_to :item
+  belongs_to :user
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :delivery_area_id, :city,:address, :building_name , :phone_number, :purchase_management
+ 
+
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_area
+
+
+  
+
+  
 end
+
