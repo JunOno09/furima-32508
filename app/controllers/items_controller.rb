bec113ac220cd21ecd37b4b94863b57083453a6b
JunOno09 <jastis.09@gmail.com>
2020-12-29 11:11:53 +0900

    ログインユーザーは購入済の商品の編集ページに行けない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user == @item.user
+    unless current_user == @item.user && @item.purchase_management.nil?
       redirect_to root_path
     end
   end
